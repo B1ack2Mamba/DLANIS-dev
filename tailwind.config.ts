e0a5f2eab2cca1b63099490b68a0630a434693fd
Alexander <sanre@D-LANIS-I.localdomain>
2025-12-09 18:29:09 +0000

    fix: build & tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,14 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  darkMode: "class",
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+const config = {
+    darkMode: "class",
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
 
 export default config;
